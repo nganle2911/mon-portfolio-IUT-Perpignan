f446c58d77db2b969b50a2d354db34cf0cce130a
Ngan LE <ngan.ltkn2911@gmail.com>
2025-11-01 00:18:47 +0100

    Finir la partie compétences

--- js/main.js
+++ js/main.js
@@ -4,9 +4,58 @@ var menu = document.getElementById("menuRight__navLink");
 var elements = menu.getElementsByClassName("navlink");
 
 for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", function() {
+    elements[i].addEventListener("click", function () {
         let actuel = document.getElementsByClassName("active");
         actuel[0].className = actuel[0].className.replace(" active", "");
         this.className += " active";
     })
 }
+
+// COMPÉTENCES
+const listeComp = [
+    {
+        nom: "HTML/CSS",
+        valeur: 75
+    },
+    {
+        nom: "JavaScript",
+        valeur: 60
+    },
+    {
+        nom: "Python",
+        valeur: 65
+    },
+    {
+        nom: "Power BI",
+        valeur: 70
+    },
+    {
+        nom: "R",
+        valeur: 55
+    }
+];
+
+// Afficher des compétences sur HTML en utilisant JS
+function afficherListeComp() {
+    let contenuHTML = "";
+
+    listeComp.map((ele, indice) => {
+        console.log(ele);
+
+        contenuHTML += `
+            <div class="skills__item">
+                <div class="skillsItem__content">
+                    <p>${ele.nom}</p>
+                    <p>${ele.valeur} %</p>
+                </div>
+                <div class="skillsItem__bar">
+                    <div class="bar__value" style="width: ${ele.valeur}%;"></div>
+                </div>
+            </div>
+        `;
+    });
+
+    document.getElementById("skills").innerHTML = contenuHTML;
+}
+
+afficherListeComp();
